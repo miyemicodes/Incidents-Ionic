77d5c8d63f3988c7b46b5c65e08a15b9578bb5af
miyemicodes <lizzymiyemi@gmail.com>
2024-06-12 00:54:43 -0700

    first commit

---
+++ src/Incidence.js
@@ -0,0 +1,31 @@
+const Incidences = [
+  {
+    id: 1,
+    img: "",
+    title: "Road Accident",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis perferendis ut vel nihil fugiat, non placeat ducimus adipisci?",
+    date: "2024-06-11",
+    time: "14:30",
+    location: "Data Center 1",
+  },
+  {
+    id: 2,
+    img: "",
+    title: "Broken Bridge",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis perferendis ut vel nihil fugiat",
+    date: "2021-10-19",
+    time: "18:30",
+    location: "Data Center rear",
+  },
+  {
+    id: 3,
+    img: "",
+    title: "System Outage",
+    description: "The system was down for maintenance.",
+    date: "2024-06-11",
+    time: "14:00",
+    location: "Remote Office",
+  },
+];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,78 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonButton,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonChip,
+  IonIcon,
+  IonText,
+} from "@ionic/react";
+import { addCircleOutline } from "ionicons/icons";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonGrid>
+            <IonRow class="ion-justify-content-between">
+              <IonCol className="ion-text-left">
+                <IonTitle className="ion-padding">Home</IonTitle>
+              </IonCol>
+              <IonCol className="ion-text-right">
+                <IonButton>
+                  <IonIcon slot="start" icon={addCircleOutline} />
+                  Add Incident
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent>
+        <IonCard>
+          <img src="" alt="" />
+
+          <IonCardHeader>
+            <IonCardTitle> Student Rioting</IonCardTitle>
+            <IonCardSubtitle>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </IonCardSubtitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            perferendis ut vel nihil fugiat, non placeat ducimus adipisci atque!
+            Placeat ratione possimus ab fugit aspernatur, nostrum quos
+            consectetur reprehenderit debitis?
+          </IonCardContent>
+
+          <IonGrid>
+            <IonRow>
+              <IonCol className="ion-text-left">
+                <IonChip color="medium">Date</IonChip>
+              </IonCol>
+              <IonCol className="ion-text-right">
+                <IonChip color="medium">Time</IonChip>
+              </IonCol>
+            </IonRow>
+
+            <div className="ion-text-center">
+              <p>Location</p>
+            </div>
+          </IonGrid>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
