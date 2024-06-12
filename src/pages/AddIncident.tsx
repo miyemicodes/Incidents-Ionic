eabce6b57066d2445ba601207dfc60c324dc70a6
miyemicodes <lizzymiyemi@gmail.com>
2024-06-12 02:39:16 -0700

    commit

--- src/pages/AddIncident.tsx
+++ src/pages/AddIncident.tsx
@@ -4,6 +4,14 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonItem,
+  IonTextarea,
+  IonText,
+  IonInput,
+  IonLabel,
+  IonSelect,
+  IonSelectOption,
+  IonButton,
 } from "@ionic/react";
 import React from "react";
 
@@ -12,11 +20,49 @@ const AddIncident: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>ADD INCIDENT</IonTitle>
+          <IonTitle>REPORT INCIDENT</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent>
-        
+        <IonItem>
+          <IonInput
+            label="Incident"
+            labelPlacement="floating"
+            placeholder="Enter text"
+          ></IonInput>
+        </IonItem>
+
+        <IonItem>
+          <IonLabel>Select Category</IonLabel>
+          <IonSelect
+            value=""
+            placeholder="Categories"
+            // onIonChange={(e) => setSelectedOption(e.detail.value)}
+          >
+            <IonSelectOption value="Fighting">Fighting</IonSelectOption>
+            <IonSelectOption value="Accident">Accident</IonSelectOption>
+            <IonSelectOption value="Riot">Riot</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+
+        <IonItem>
+          <IonTextarea
+            label="Description"
+            labelPlacement="floating"
+            placeholder="Enter description"
+          >
+            <div slot="label">
+              Comments <IonText color="danger">(Required)</IonText>
+            </div>
+          </IonTextarea>
+        </IonItem>
+
+        <IonItem>
+          <IonInput label="Add Image" labelPlacement="floating"></IonInput>
+        </IonItem>
+
+        <IonButton expand="block">Submit</IonButton>
       </IonContent>
     </IonPage>
   );
